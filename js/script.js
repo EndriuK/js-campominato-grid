0f4481cfcd2f1bf593a9b81fa197a8d306e2182f
EndriuK <endrit.kaskija@gmail.com>
2024-07-05 17:58:20 +0200

    [UPD] fine funzione btn + vari stili css

--- js/script.js
+++ js/script.js
@@ -1,25 +1,35 @@
 // definizione della funzione che mi crea i quadrati della griglia
 function createSquare(value) {
   let currentElement = document.createElement("div");
-  // metto il numero progressivo
+  // gli metto il numero progressivo
   currentElement.innerText = value + 1;
   // assegnazione della classe square all'elemento
   currentElement.classList.add("square");
+
   // restituisco il quadrato contenuto della variabile currentElement
   return currentElement;
 }
-
-// recupero pulsante dal dom
+// recupero il pulsante dal dom
 const btn = document.getElementById("start");
 
 // recupero l'elemento che conterrà la mia griglia
 const grid = document.getElementById("grid");
 
-//creo funzione bottone per lo start
 btn.addEventListener("click", function () {
-  //eseguo ciclo di 100 iterazione per creare la griglia 10x10
+  // eseguo un cliclo di 100 iterazioni per creare una griglia x10
   for (let i = 0; i < 100; i++) {
-    //creo il singolo quadrato chiamando la funzione createSquare
-    let createSquare = createSquare(i);
+    // creo il singolo quadrato chiamando la funzione createSquare
+    let currentSquare = createSquare(i);
+
+    // aggiungo l'evento click al quadrato creato
+    currentSquare.addEventListener("click", function () {
+      //   console.log(this);
+
+      this.classList.add("clicked-sky");
+    });
+
+    // console.log(i + 1);
+    // appendo il quadrato appena creato all'interno della griglia presa precedentemente
+    grid.append(currentSquare);
   }
 });
